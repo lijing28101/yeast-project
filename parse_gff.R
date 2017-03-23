2efbb1a8bad4bb3382a1adc49d9f622d4fd97e9c
lijing28101 <jingli@iastate.edu>
2017-03-23 14:01:49 -0500

    Add files via upload

---
+++ parse_gff.R
@@ -0,0 +1,35 @@
+getAttributeField <- function (x, field, attrsep = ";") {
+     s = strsplit(x, split = attrsep, fixed = TRUE)
+     sapply(s, function(atts) {
+         a = strsplit(atts, split = "=", fixed = TRUE)
+         m = match(field, sapply(a, "[", 1))
+         if (!is.na(m)) {
+             rv = a[[m]][2]
+         }
+         else {
+             rv = as.character(NA)
+         }
+         return(rv)
+     })
+}
+
+gffRead <- function(gffFile, nrows = -1) {
+     cat("Reading ", gffFile, ": ", sep="")
+     gff = read.table(gffFile, sep="\t", as.is=TRUE, quote="",
+     header=FALSE, comment.char="#", nrows = nrows,
+     colClasses=c("character", "character", "character", "integer",  
+"integer",
+     "character", "character", "character", "character"))
+     colnames(gff) = c("seqname", "source", "feature", "start", "end",
+             "score", "strand", "frame", "attributes")
+     cat("found", nrow(gff), "rows with classes:",
+         paste(sapply(gff, class), collapse=", "), "\n")
+     stopifnot(!any(is.na(gff$start)), !any(is.na(gff$end)))
+     return(gff)
+}
+
+species.gff <- gffRead("gff/species.gff")
+species.gff_CDS <- subset(species.gff,feature=="CDS")
+species.gff_CDS2 <- subset(species.gff_CDS,select=-(attributes))
+species.gff_CDS2$Parent <- getAttributeField(CDS$attributes,"Parent")
+
