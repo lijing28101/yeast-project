937df2b2e53743398154816cb1cc1e7a1efbaaf1
Zebulun Arendsee <arendsee@iastate.edu>
2017-03-24 14:49:00 -0500

    Fix docopt handling

--- parse_gff.R
+++ parse_gff.R
@@ -3,17 +3,13 @@
 require(docopt)
 
 "Usage:
-    parse_gff.R [ -h -i <input> -o <output> -f <feature> -a1 <attribute1> -a2 <attribute2> -a3 <attribute3> ]
+    parse_gff.R [ -i <input> ] [ -o <output> ] [ -f <feature> ] [ -a <attribute> ]
 
 Options:
-  -h --help Show this screen
   -i Input GFF file
   -o Output GFF file
   -f Feature of gene
-  -a1 Attribute1 of gene
-  -a2 Attribute2 of gene
-  -a3 Attribute3 of gene
-
+  -a Attributes (comma delimited, e.g. 'ID,Parent'
 " -> doc
 
 opts <- docopt(doc)
@@ -24,7 +20,7 @@ opts <- docopt(doc)
 
 getAttributeField <- function (x, field, attrsep = ";") {
   regex <- sprintf('.*%s=([^;]+).*', field)
-  regex_log <- sprintf('%s=[^;]+', field) 
+  regex_log <- sprintf('%s=[^;]+', field)
   if( !all(grepl(regex_log, x)) ){
     warning(sprintf("Incorrect formatting of GFF file, could not find pattern '%s'", regex_log))
   }
@@ -36,7 +32,8 @@ gffRead <- function(gffFile, nrows = -1) {
      gff = read.table(
        gffFile,
        sep="\t",
-       as.is=TRUE,
+       # as.is=TRUE,
+       stringsAsFactors=FALSE,
        quote="",
        header=FALSE,
        comment.char="#",
@@ -62,9 +59,16 @@ if(!file.exists(opts$i)){
 species.gff <- gffRead(opts$i)
 species.gff_F <- subset(species.gff,feature==opts$f)
 species.gff_F2 <- subset(species.gff_F,select=-(attributes))
-species.gff_F2$opts$a1 <- getAttributeField(species.gff_F$attributes,opts$a1)
-species.gff_F2$opts$a2 <- getAttributeField(species.gff_F$attributes,opts$a2)
-species.gff_F2$opts$a3 <- getAttributeField(species.gff_F$attributes,opts$a3)
 
-write.table(species.gff_F2,opts$o,sep="\t",row.names=FALSE,col.names=FALSE,quote=FALSE)
+for(a in strsplit(opts$a, ",")[[1]]){
+  species.gff_F2[[a]] <- getAttributeField(species.gff_F$attributes,a)
+}
 
+write.table(
+  species.gff_F2,
+  opts$o,
+  sep       = "\t",
+  row.names = FALSE,
+  col.names = FALSE,
+  quote     = FALSE
+)
